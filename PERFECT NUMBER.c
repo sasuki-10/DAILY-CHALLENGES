12502cf159eb8a3b0252f90ea37551f7cd68fdfb
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-05 23:37:46 +0530

    PERFECT NUMBER.c

---
+++ PERFECT NUMBER.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int num,sum=0;                 //perfect number:For example(MULTIPLE OF 6: 1;2;3;6) ignoring its own value then it will be equal to its own number.
+    printf("ENTER THE NUMBER:");     //the sum of 1+3+2=6;
+    scanf("%d",&num);
+    for( int i=1;i<num;i++)
+    {
+       if(num%i==0)
+        {
+         sum=sum+i;  
+        }
+    }
+    if(num==sum)
+    {
+        printf("GIVEN NUMBER IS PERFECT");
+    }
+    else
+    {
+       printf("GIVEN NUMBER IS NOT PERFECT"); 
+    }
+    return 0;
+}
