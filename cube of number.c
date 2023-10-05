25f8ba31ba1107228bff5d7d63498ff11a65cbe0
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-05 23:49:20 +0530

    cube of number.c

---
+++ cube of number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int num,n;                 
+    printf("ENTER THE NUMBER:");    
+    scanf("%d",&num);
+    for( int i=1;i<=num;i++)
+    {
+        printf(" %0.1f",pow(i,3));
+    }
+    
+    return 0;
+}
