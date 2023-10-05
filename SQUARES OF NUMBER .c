406b3ef37586c060398849ed25cb1961025b141b
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-05 23:45:46 +0530

    SQUARES OF NUMBER .c

---
+++ SQUARES OF NUMBER .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int num,n;                 
+    printf("ENTER THE NUMBER:");    
+    scanf("%d",&num);
+    for( int i=1;i<=num;i++)
+    {
+        printf(" %0.1f",pow(i,i));
+    }
+    
+    return 0;
+}
