5c3a6a7e87537416e3a585118d26d6d3074a02b5
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-06 22:15:59 +0530

    FACTORIAL.c

---
+++ FACTORIAL.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  int num,fact=1;
+  printf("ENTER THE NUMBER:");
+  scanf("%d",&num);
+   
+  for(int i=1;i<=num;i++)
+    {
+      fact=fact*i;
+    } 
+    printf("FACTORIAL OF THE NUMBER:%d",fact);
+} 
+    
