ae1b63b621736edbc4370634b618ceb3eb57d847
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-06 22:58:58 +0530

    FOR(SUPERMARKET).c

---
+++ FOR(SUPERMARKET).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{                                                                                //concept says that: in supermarket the cashier does not know the total items in your bucket...
+  int n=0,sum=0;                                                                //at the end when there is no item left cashier will enter -1....
+  
+  for( ;n>=0;) 
+    {
+       sum=sum+n;
+      printf("ENTER THE PRICE:");
+      scanf("%d",&n);
+       
+    } 
+     printf("total of the prices: %d",sum);
+} 
+    
