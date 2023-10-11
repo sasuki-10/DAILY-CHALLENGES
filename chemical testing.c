b8f7cbf7b5ab5e88dbf65bb22deddb5326646b3f
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-11 23:58:46 +0530

    chemical testing.c

---
+++ chemical testing.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  printf("\n(hEm_L@b");
+  printf("\nWELCOME TO CHEM LAB TESTING");
+  float pH,chemical;
+
+   printf("\nENTER THE VALUE OF CHEMICAL: ");
+   scanf("%f",&pH);
+  if(pH>7.0)
+    printf("CHEMICAL IS BASIC ☺");
+    
+         else if(pH<7.0)
+         printf("CHEMICAL IS Acidic ☺");
+          
+             else
+              printf("CHEMICAL IS Neutral ☺");
+
+    printf("\nEnter the name of chemical: ");
+    scanf("%d",&chemical);
+
+}
