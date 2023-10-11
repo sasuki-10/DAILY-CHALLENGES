87e69783b715c609cec5cf55fd2b152eb5f30fdf
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-11 23:49:36 +0530

    WEEKS.c

---
+++ WEEKS.c
@@ -0,0 +1,46 @@
+//checking switch statement..
+#include<stdio.h>
+int main()
+{
+ int num;
+ printf("ENTER YOUR NUMBER: ");
+ scanf("%d",&num);
+   switch(num)
+  {
+     case 1: (num==1);
+ 
+     printf("sunday");
+ 
+     case 2: (num==2);
+ 
+     printf(" monday");
+ 
+     case 3: (num==3);
+ 
+     printf(" tuesday");
+ 
+    case 4: (num==4);
+ 
+     printf(" wednesday");
+ 
+     case 5: (num==5);
+ 
+     printf(" thursday");
+ 
+    case 6: (num==6);
+ 
+     printf(" friday");
+ 
+    case 7: (num==7);
+ 
+     printf(" saturday");
+ 
+    default:
+ 
+     printf(" not in week");
+     
+    }
+ 
+ return 0;
+ 
+}
