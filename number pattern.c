1b33ae00f2c45d2a212cc90661f95addc36be147
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-13 23:03:00 +0530

    number pattern.c

---
+++ number pattern.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("ENTER THE VALUE:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=5;j>=n;j--)
+        {
+            printf("%d",j);
+            
+        }
+        
+        printf("\n");
+    }
+    
+}
