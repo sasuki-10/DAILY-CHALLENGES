5f029dc18d872e702612be9e57d6ba63697208e8
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-15 23:59:34 +0530

    series.c

---
+++ series.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    int sum=0,i;
+    printf("Enter the range of number:");
+    scanf("%d",&n);
+    
+    int p;             //for example if p=1,1+11+111+1111......
+    printf("Enter the value of number:");
+    scanf("%d",&p);
+   
+   for(i=1;i<=n;i++)
+    {
+            sum+=p;
+            p=(p*10)+9;
+    }
+    printf("The sum of the series = %d",sum);
+}
