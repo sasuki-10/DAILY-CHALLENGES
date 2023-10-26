1e334d8c359fd2cd230eaedf0fec43a1a600aa6f
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-26 22:44:38 +0530

    Create string length.c

---
+++ string length.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char s[100],i=0;
+	printf("ENTER THE STRING:");
+	gets(s);                       
+    for(i=0;s[i]!=NULL;++i);
+    
+	printf("Total length of string:%d",i);
+	// also includes if there is any gap between entered sentence.
+	return 0;
+}
