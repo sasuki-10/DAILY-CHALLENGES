c059ca0523ff1e18af06da39b8744b03b7ea8ad4
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-26 23:08:59 +0530

    Create lowercase-->uppercase.c

---
+++ lowercase-->uppercase.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	char f[50],s[50];
+	int i=0,temp,c;
+	printf("Enter the string:");
+	gets(f[i]);
+	while(f[i]!=NULL);
+  i++;
+	for(c=0;c<=i;c++)
+	{
+		temp=f[c];
+		if(temp>=97&&temp<=122)
+		{
+			s[c]=temp-32;
+		}
+		else
+		{
+			s[c]=f[c];
+		}
+	}
+	printf("STRING{UPPER CASE}=%s",s);
+}
