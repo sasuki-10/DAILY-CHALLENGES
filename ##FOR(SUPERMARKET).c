97d3fb55265297fd887712958c78f622a553d18f
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-06 22:07:10 +0530

    ##FOR(SUPERMARKET).c

--- ##FOR(SUPERMARKET).c
+++ ##FOR(SUPERMARKET).c
@@ -2,7 +2,6 @@
 int main()
 {                                                                                //concept says that: in supermarket the cashier does not know the total items in your bucket...
   int n=0,sum=0;                                                                //at the end when there is no item left cashier will enter -1....
-  
   for( ;n>=0;) 
     {
        sum=sum+n;
