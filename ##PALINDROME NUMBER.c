3a264b94cf4e10940106650d67849bc4f1fc02b8
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-06 22:08:18 +0530

    ##PALINDROME NUMBER.c

--- ##PALINDROME NUMBER.c
+++ ##PALINDROME NUMBER.c
@@ -1,26 +1,24 @@
 #include<stdio.h>
-int main()
-{
-int n,r,sum=0;
-int temp;
-  printf("Enter number to check palindrome or not\n");
-  scanf("%d",&n);
-  temp = n;
+  int main()
+  {
+   int n,r,sum=0;
+   int temp;
+   printf("Enter number to check palindrome or not\n");
+   scanf("%d",&n);
+   temp = n;
    while(n>0)
   {
-   r = n%10;
-   sum = (sum*10)+r;
-   n = n/10;
+    r = n%10;
+    sum = (sum*10)+r;
+    n = n/10;
   }
 if(temp == sum)
      {
-         printf("Number is palindrome \n");
-         
+         printf("Number is palindrome \n");   
      }
 else 
    {
-    printf("Number is NOT palindrome\n");
-       
+    printf("Number is NOT palindrome\n");   
    }
 return 0;
 }
