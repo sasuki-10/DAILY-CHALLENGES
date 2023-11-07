c37f923b086535e4fbdcf3a04403738828aed6b2
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-07 22:53:32 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ char a[5][100],i;
+ printf("********ENTER NAMES********");
+ for(i=0;i<5;i++)
+ {
+  gets(a[i]);
+  	
+ }
+ printf("the names are:");
+ for(i=0;i<5;i++)
+ {
+ 	puts(a[i]);
+ }
+ return 0;
+}
