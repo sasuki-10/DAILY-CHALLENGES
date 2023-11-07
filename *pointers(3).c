30faffe31419957e683b5ee3f429e03ef5e8d93e
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-07 18:10:35 +0530

    *pointers(3).c

---
+++ *pointers(3).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int *iptr;
+  int *fptr;
+  int *cptr;
+  int *dptr;
+  printf("total size of integer value pointer:%d\n",sizeof(iptr));
+  printf("total size of float value pointer:%f\n",sizeof(fptr));
+  printf("total size of character value pointer:%c\n",sizeof(cptr));
+  printf("total size of double value pointer:%d\n",sizeof(dptr));
+}
