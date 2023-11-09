6ed88e246b918fb4714b6c231a4055c5e1c9c94d
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-09 22:34:29 +0530

    string length.c

--- string length.c
+++ string length.c
@@ -3,9 +3,9 @@ int main()
 {
 	char s[100],i=0;
 	printf("ENTER THE STRING:");
-	gets(s);                       
-    for(i=0;s[i]!=NULL;++i);
-    
+	gets(s); 
+	
+        for(i=0;s[i]!=NULL;++i);
 	printf("Total length of string:%d",i);
 	// also includes if there is any gap between entered sentence.
 	return 0;
