92e77f2ba11060690a1973e7cc3e2910790265ca
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-09 22:33:50 +0530

    series.c

--- series.c
+++ series.c
@@ -5,11 +5,9 @@ int main()
     int sum=0,i;
     printf("Enter the range of number:");
     scanf("%d",&n);
-    
     int p;             //for example if p=1,1+11+111+1111......
     printf("Enter the value of number:");
     scanf("%d",&p);
-   
    for(i=1;i<=n;i++)
     {
             sum+=p;
