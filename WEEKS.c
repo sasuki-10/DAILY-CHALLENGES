7abb639820ad7feaac2b0fb25987505e68425bb7
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-09 22:29:11 +0530

    WEEKS.c

--- WEEKS.c
+++ WEEKS.c
@@ -7,31 +7,31 @@ int main()
  scanf("%d",&num);
    switch(num)
   {
-     case 1: (num==1);
+     case 1: 
  
      printf("sunday");
  
-     case 2: (num==2);
+     case 2: 
  
      printf(" monday");
  
-     case 3: (num==3);
+     case 3: 
  
      printf(" tuesday");
  
-    case 4: (num==4);
+    case 4: 
  
      printf(" wednesday");
  
-     case 5: (num==5);
+     case 5: 
  
      printf(" thursday");
  
-    case 6: (num==6);
+    case 6: 
  
      printf(" friday");
  
-    case 7: (num==7);
+    case 7: 
  
      printf(" saturday");
  
