5fe8aa61fc530f098d5711b8669472541512f6ed
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-09 22:27:07 +0530

    Series.c

--- Series.c
+++ Series.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <math.h>
-
 int main()
 {
     int counter,f_coun;
@@ -8,19 +6,15 @@ int main()
     printf("\tEQUATION SERIES : 1- X^2/2! + X^4/4! - X^6/6! + X^8/8! - X^10/10!");
     printf("\n \tENTER VALUE OF X : ");
     scanf("%f",&x);
-
     for(counter=0, power=0; power<=10; counter++,power=power+2)
     {
         fact=1;
         //Factorial of POWER value.
-        
         for(f_coun=power; f_coun>=1; f_coun--)
             fact *= f_coun;
         //The main equation for sum of series is...
-        
         sum=sum+(pow(-1,counter)*(pow(x,power)/fact));
     }
-
     printf("SUM : %f",sum);
 
 }
