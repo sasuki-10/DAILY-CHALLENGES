cc793d027fb15c27910f379b0a17b8395c56a6ea
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-22 22:48:26 +0530

    structure(1).c

---
+++ structure(1).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+struct student
+{
+	char rn[50];
+	char name[50];
+	int marks[5];
+	float per;
+}s1;
+
+int main()
+{
+	int i;
+	struct student;
+	printf("Enter your roll number: ");
+	gets(s1.rn);
+	printf("Enter your name:");
+	gets(s1.name);	
+}
